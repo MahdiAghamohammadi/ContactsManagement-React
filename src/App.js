da2d0e2d1c26127d2c1e6900c24bf6d1157bfa3b
mahdi <aghamohammadi.mehdi15@gmail.com>
2023-07-20 18:47:38 +0330

    added necessary tools for project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Contact Management</p>
+
+        {/* <span className="fa fa-user"></span> */}
+        <button className="btn btn-primary">click me</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
