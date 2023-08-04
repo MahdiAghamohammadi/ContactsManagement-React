76e60ade42493c0439733bf8e55776f62d7212e9
mahdi <aghamohammadi.mehdi15@gmail.com>
2023-08-04 21:57:29 +0330

    complete the edit contact component

--- src/components/Contacts/EditContact.jsx
+++ src/components/Contacts/EditContact.jsx
@@ -1,7 +1,211 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+import {
+  getContact,
+  getAllGroups,
+  updateContact,
+} from "../../services/contactService";
+import { Spinner } from "../";
+import { COMMENT, ORANGE, PURPLE } from "../../helpers/colors";
+
 const EditContact = () => {
+  const { contactId } = useParams();
+  const navigate = useNavigate();
+
+  const [state, setState] = useState({
+    loading: false,
+    contact: {
+      fullname: "",
+      photo: "",
+      mobile: "",
+      email: "",
+      job: "",
+      group: "",
+    },
+    groups: [],
+  });
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setState({ ...state, loading: true });
+        const { data: contactData } = await getContact(contactId);
+        const { data: groupsData } = await getAllGroups();
+
+        setState({
+          ...state,
+          loading: false,
+          contact: contactData,
+          groups: groupsData,
+        });
+      } catch (err) {
+        console.log(err.message);
+        setState({ ...state, loading: false });
+      }
+    })();
+  }, []);
+
+  const contactUpdate = (e) => {
+    setState({
+      ...state,
+      contact: {
+        ...state.contact,
+        [e.target.name]: [e.target.value],
+      },
+    });
+  };
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      setState({ ...state, loading: true });
+      const { data } = await updateContact(state.contact, contactId);
+      setState({ ...state, loading: false });
+      if (data) {
+        navigate("/contacts");
+      }
+    } catch (e) {
+      console.log(e.message);
+      setState({ ...state, loading: false });
+    }
+  };
+
+  const { loading, contact, groups } = state;
+
   return (
     <>
-      <h2>Edit Contact</h2>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <section className="p-3">
+            <div className="container">
+              <div className="row my-2">
+                <div className="col text-center">
+                  <p className="h4 fw-bold" style={{ color: ORANGE }}>
+                    Edit Contact
+                  </p>
+                </div>
+              </div>
+              <hr style={{ backgroundColor: ORANGE }} />
+              <div
+                className="row p-2 w-75 mx-auto align-items-center"
+                style={{ backgroundColor: "#44475a", borderRadius: "1em" }}
+              >
+                <div className="col-md-8">
+                  <form onSubmit={submitForm}>
+                    <div className="mb-2">
+                      <input
+                        name="fullname"
+                        type="text"
+                        className="form-control"
+                        value={contact.fullname}
+                        onChange={contactUpdate}
+                        required={true}
+                        placeholder="Full Name"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="photo"
+                        type="text"
+                        value={contact.photo}
+                        onChange={contactUpdate}
+                        className="form-control"
+                        required={true}
+                        placeholder="Image"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="mobile"
+                        type="number"
+                        className="form-control"
+                        value={contact.mobile}
+                        onChange={contactUpdate}
+                        required={true}
+                        placeholder="Mobile"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="email"
+                        type="email"
+                        className="form-control"
+                        value={contact.email}
+                        onChange={contactUpdate}
+                        required={true}
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="job"
+                        type="text"
+                        className="form-control"
+                        value={contact.job}
+                        onChange={contactUpdate}
+                        required={true}
+                        placeholder="Job"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <select
+                        name="group"
+                        value={contact.group}
+                        onChange={contactUpdate}
+                        required={true}
+                        className="form-control"
+                      >
+                        <option value="">Select Group</option>
+                        {groups.length > 0 &&
+                          groups.map((group) => (
+                            <option key={group.id} value={group.id}>
+                              {group.name}
+                            </option>
+                          ))}
+                      </select>
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="submit"
+                        className="btn"
+                        style={{ backgroundColor: PURPLE }}
+                        value="Edit Contact"
+                      />
+                      <Link
+                        to={"/contacts"}
+                        className="btn mx-2"
+                        style={{ backgroundColor: COMMENT }}
+                      >
+                        Cancel
+                      </Link>
+                    </div>
+                  </form>
+                </div>
+                <div className="col-md-4">
+                  <img
+                    src={contact.photo}
+                    className="img-fluid rounded"
+                    style={{ border: `1px solid ${PURPLE}` }}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="text-center mt-1">
+              <img
+                src={require("../../assets/man-taking-note.png")}
+                height="300px"
+                style={{ opacity: "60%" }}
+                alt=""
+              />
+            </div>
+          </section>
+        </>
+      )}
     </>
   );
 };
