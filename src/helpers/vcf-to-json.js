21a9470002b5a04740fd1e55567538a25de1ef7d
Mahdi Aghamohammadi <aghamohammadi.mehdi15@gmail.com>
2023-12-01 20:55:59 +0330

    fix: remove unused packages

--- src/helpers/vcf-to-json.js
+++ src/helpers/vcf-to-json.js
@@ -1,48 +1,47 @@
-import quotedPrintable from "quoted-printable";
-import utf8 from "utf8";
-
 export function convertVCFToJson(vcfContent) {
-    const contacts = [];
-    let currentContact = {};
+  const contacts = [];
+  let currentContact = {};
 
-    vcfContent.split("\n").forEach(line => {
-        if (line.startsWith("BEGIN:VCARD")) {
-            currentContact = {};
-        } else if (line.startsWith("END:VCARD")) {
-            contacts.push(currentContact);
-        } else if (line.includes(":")) {
-            const parts = line.split(":");
-            const key = parts[0];
-            const value = parts[1] || "";
-            const isUtf8 = key.includes("CHARSET=UTF-8");
-            if (isUtf8) {
-                currentContact[key] = quotedPrintableDecode(value);
-            } else {
-                currentContact[key] = value;
-            }
-        }
-    });
+  vcfContent.split("\n").forEach((line) => {
+    if (line.startsWith("BEGIN:VCARD")) {
+      currentContact = {};
+    } else if (line.startsWith("END:VCARD")) {
+      contacts.push(currentContact);
+    } else if (line.includes(":")) {
+      const parts = line.split(":");
+      const key = parts[0];
+      const value = parts[1] || "";
+      const isUtf8 = key.includes("CHARSET=UTF-8");
+      if (isUtf8) {
+        currentContact[key] = quotedPrintableDecode(value);
+      } else {
+        currentContact[key] = value;
+      }
+    }
+  });
 
-    return contacts.map(contact => {
-        let mobile = contact["TEL;CELL;PREF"]?.trim().split("\r\n")[0] || contact["TEL;CELL"]?.trim().split("\r\n")[0];
+  return contacts.map((contact) => {
+    let mobile =
+      contact["TEL;CELL;PREF"]?.trim().split("\r\n")[0] ||
+      contact["TEL;CELL"]?.trim().split("\r\n")[0];
 
-        return {
-            fullname: contact["FN"]?.trim(),
-            mobile  : mobile.replace(/-/g, '').replace(/^\+98/, '0'),
-            photo   : "/images/avatar.png",
-            email   : "",
-            job     : "",
-            group   : ""
-        };
-    });
+    return {
+      fullname: contact["FN"]?.trim(),
+      mobile: mobile.replace(/-/g, "").replace(/^\+98/, "0"),
+      photo: "/images/avatar.png",
+      email: "",
+      job: "",
+      group: "",
+    };
+  });
 }
 
 export function quotedPrintableDecode(str) {
-    const percentEncodedStr = str.replace(/=([0-9A-F]{2})/g, "%$1");
-    try {
-        return decodeURIComponent(percentEncodedStr);
-    } catch (e) {
-        console.error("Error in UTF-8 decoding: ", e);
-        return "";
-    }
+  const percentEncodedStr = str.replace(/=([0-9A-F]{2})/g, "%$1");
+  try {
+    return decodeURIComponent(percentEncodedStr);
+  } catch (e) {
+    console.error("Error in UTF-8 decoding: ", e);
+    return "";
+  }
 }
